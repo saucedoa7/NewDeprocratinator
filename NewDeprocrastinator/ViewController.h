64d672d68b5a8b2af5e4b89e305956c6343bea53
Albert Saucedo <saucedoa7@gmail.com>
2014-10-05 22:08:58 -0500

    Initial Commit

---
+++ NewDeprocrastinator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewDeprocrastinator
+//
+//  Created by Albert Saucedo on 10/5/14.
+//  Copyright (c) 2014 Albert Saucedo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NewDeprocrastinator/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NewDeprocrastinator
+//
+//  Created by Albert Saucedo on 10/5/14.
+//  Copyright (c) 2014 Albert Saucedo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
