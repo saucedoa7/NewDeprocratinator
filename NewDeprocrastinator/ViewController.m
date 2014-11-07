99d0c2d2d432f6289b1d84deebba8121855c060d
Albert Saucedo <saucedoa7@gmail.com>
2014-11-06 21:18:08 -0600

    Resign first responder works

--- NewDeprocrastinator/ViewController.m
+++ NewDeprocrastinator/ViewController.m
@@ -6,7 +6,7 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()<UITableViewDataSource, UITableViewDelegate>
+@interface ViewController ()<UITableViewDataSource, UITableViewDelegate,UITextFieldDelegate>
 @property (weak, nonatomic) IBOutlet UITableView *toDoTableView;
 @property (weak, nonatomic) IBOutlet UITextField *todoTextField;
 @property (weak, nonatomic) IBOutlet UIBarButtonItem *editButton;
@@ -17,8 +17,11 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
+    self.toDoTableView.backgroundColor = [UIColor colorWithRed:0.25 green:0.6 blue:0.38 alpha:1];
     self.todos = [[NSMutableArray alloc]initWithObjects:@"One",@"Two",@"Three", @"Four", nil];
+
+    //Hide KB 1/2
+    self.todoTextField.delegate = self;
 }
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
@@ -28,6 +31,7 @@
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"todoCellID"];
     cell.textLabel.text = [self.todos objectAtIndex:indexPath.row];
+
     return cell;
 }
 
@@ -49,7 +53,7 @@
 }
 
 -(NSString *)tableView:(UITableView *)tableView titleForDeleteConfirmationButtonForRowAtIndexPath:(NSIndexPath *)indexPath{
-    return @"Delete it";
+    return @"Delete Bruh";
 }
 
 - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
@@ -75,12 +79,28 @@
     } else if ([self.editButton.title isEqualToString:@"Edit"]){
         cell.textLabel.textColor = [UIColor colorWithRed:0.33 green:0.84 blue:0.49 alpha:1];
     }
-
-    NSLog(@"Selected");
 }
 
 -(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
     return 100;
 }
+
+-(void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)sourceIndexPath toIndexPath:(NSIndexPath *)destinationIndexPath{
+
+
+    NSMutableArray *originArray = [self.todos objectAtIndex:sourceIndexPath.section];
+    NSMutableArray *destinyArray = [self.todos objectAtIndex:destinationIndexPath.section];
+
+    [destinyArray addObject:[originArray objectAtIndex:sourceIndexPath.row]];
+    [originArray removeObjectAtIndex:sourceIndexPath.row];
+    [self.toDoTableView reloadData];
+}
+
+    //Hide KB 2/2
+-(BOOL)textFieldShouldReturn:(UITextField *)textField{
+    [self.todoTextField resignFirstResponder];
+    return YES;
+}
+
 
 @end
